d8c4b855f26bd882b13a18acba668584508120e3
Simon Malpel (SimonOriginal) <simon.malpel@orange.fr>
2019-08-27 16:22:56 +0200

    Fix slug injection in controller

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
     require $path.'/autoload.php';
 
     foreach ($Configuration->routes as $rts) {
-        $router->match($rts['type'], $rts['pattern'], function($slug = null) use ($route, $rts) { ($route->load($rts['controller']))->index($_SERVER['REQUEST_METHOD']); });
+        $router->match($rts['type'], $rts['pattern'], function($slug = null) use ($route, $rts) { ($route->load($rts['controller']))->index($_SERVER['REQUEST_METHOD'], (!empty($slug)? $slug: null)); });
     }
 
     $router->set404(function() use ($route) { ($route->load('Errors'))->NotFound(); });
