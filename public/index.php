77c6c28808b6bc112d68a3df9ceaa2885d575388
Simon Malpel <simon.malpel@orange.fr>
2019-10-11 11:17:34 +0200

    Router can call any controller function (not only index)

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@
     require $path.'/autoload.php';
 
     foreach ($Configuration->routes as $rts) {
-        $router->match($rts['type'], $rts['pattern'], function($slug = null) use ($route, $rts) { ($route->load($rts['controller']))->index($_SERVER['REQUEST_METHOD'], (!empty($slug)? $slug: null)); });
+        [ $rts['controller'], $rts['function'] ] = explode('::', $rts['controller']);
+        $router->match($rts['type'], $rts['pattern'], function($slug = null) use ($route, $rts) { ($route->load($rts['controller']))->{$rts['function']}($_SERVER['REQUEST_METHOD'], (!empty($slug)? $slug: null)); });
     }
 
     $router->set404(function() use ($route) { ($route->load('Errors'))->NotFound(); });
