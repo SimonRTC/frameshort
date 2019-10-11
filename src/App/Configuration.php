e0a67580341365ff577ad8de1cc1f69e208ae23e
Simon Malpel <simon.malpel@orange.fr>
2019-10-11 19:38:05 +0200

    Clean database config getter

--- src/App/Configuration.php
+++ src/App/Configuration.php
@@ -6,18 +6,40 @@ use Symfony\Component\Yaml\Yaml;
 
 class Configuration {
 
+    private $Path;
     public $routes;
+    public $Databases;
 
     public function __construct() {
+        $this->Path         = realpath(__DIR__ . '/..');
         $this->routes       = $this->GetRoutes();
+        $this->Databases    = $this->GetDatabases();
+    }
+
+    private function GetDatabases(): ?array {
+        $parse = file_get_contents( $this->Path . '/config/databases.json' );
+        $parse = json_decode($parse, true);
+        $parse = $this->ParseComments($parse);
+        return (!$parse? null: $parse);
     }
     
-    private function GetRoutes(): array {
-        $parse = file_get_contents( realpath(__DIR__ . '/..') . '/config/routes.yaml' );
+    private function GetRoutes(): ?array {
+        $parse = file_get_contents( $this->Path . '/config/routes.yaml' );
         $parse = Yaml::parse($parse);
-        return (!$parse? []: $parse);
+        return (!$parse? null: $parse);
     }
     
+    public function ParseComments(?array $parses): ?array {
+        foreach ($parses as $key=>$parse) {
+            foreach ($parse as $k=>$p) {
+                if ($k == '_comment') {
+                    unset($parses[$key][$k]);
+                }
+            }
+        }
+        return $parses;
+    }
+
 }
 
 ?>
