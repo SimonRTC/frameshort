dae6fb17765a8bc51752c85c0b3687f0e00be937
Simon Malpel <simon.malpel@orange.fr>
2020-05-25 14:07:10 +0200

    (Fix) Bootstrap JS loading

--- src/Components/footer.php
+++ src/Components/footer.php
@@ -1,7 +1,7 @@
 
 
-        <script src="/assets/js/bootstrap.min"></script>
-        <script src="/assets/js/bootstrap.bundle.min"></script>
+        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="/assets/js/bootstrap.bundle.min.js"></script>
 
     </body>
 </html> 
